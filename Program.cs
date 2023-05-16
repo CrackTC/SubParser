62c48acb003d9b1ca47be7b0f4089c9abf3e9e17
CrackTC <chenrke369@gmail.com>
2023-05-16 15:37:20 +0800

    remove unused methods

--- Program.cs
+++ Program.cs
@@ -1,6 +1,4 @@
-﻿using YamlDotNet.Serialization;
-
-namespace top.cracktc.SubParser
+﻿namespace top.cracktc.SubParser
 {
 
     internal class Program
@@ -14,16 +12,8 @@ namespace top.cracktc.SubParser
             return (port is > 0 and < 65536) ? port : defaultPort;
         }
 
-        private static ClashConfig GetCustomConfig()
-        {
-            string filePath = Environment.GetEnvironmentVariable("CONFIG_PATH") ?? "config.yml";
-            return new Deserializer().Deserialize<ClashConfig>(File.ReadAllText(filePath));
-        }
-
         private static void Main()
         {
-            var config = GetCustomConfig();
-
             string customConfigPath = Environment.GetEnvironmentVariable("CONFIG_PATH") ?? "./config/config.yml";
             var handler = new RequestHandler(customConfigPath);
 
